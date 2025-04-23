2a628adb1ec6109242035b11f784f00e76ceb1d4
notMarque <119892718+notMarque@users.noreply.github.com>
2025-04-23 14:29:03 -0400

    PWM stuff added

--- blink.c
+++ blink.c
@@ -1,27 +1,39 @@
-#include "pico/stdlib.h"
+/**
+ * Test file to check upload and PWM function 
+ * NOTE: must add  hardware_pwm to target_link_libraries in CMakeLists 
+ */
 
-#define LED_DELAY_MS 500
-#define BLUE_LED 21
-#define GREEN_LED 22
-
-
-
-int main() {
-    gpio_init(BLUE_LED);
-    gpio_set_dir(BLUE_LED, GPIO_OUT);
-
-    gpio_init(GREEN_LED);
-    gpio_set_dir(GREEN_LED, GPIO_OUT);
-
-    while (true) {
-        gpio_put(BLUE_LED, true);
-        gpio_put(GREEN_LED, false);
-
-        sleep_ms(LED_DELAY_MS);
-
-        gpio_put(BLUE_LED, false);
-        gpio_put(GREEN_LED, true);
-
-        sleep_ms(LED_DELAY_MS);
-    }
-}
+ #include "pico/stdlib.h"
+ #include "hardware/pwm.h"
+ 
+ #define LED_DELAY_MS 500
+ #define BLUE_LED 21 //2B
+ #define GREEN_LED 22
+ 
+ 
+ 
+ int main() {
+     gpio_set_function(BLUE_LED, GPIO_FUNC_PWM);
+     uint slice = pwm_gpio_to_slice_num (BLUE_LED); 
+     uint channel = pwm_gpio_to_channel (BLUE_LED);
+     pwm_set_wrap(slice, 12500);
+     pwm_set_enabled (slice, true);
+ 
+     pwm_set_chan_level(slice, channel, 3000);
+ 
+     gpio_init(GREEN_LED);
+     gpio_set_dir(GREEN_LED, GPIO_OUT);
+ 
+     while (true) {
+         pwm_set_gpio_level(BLUE_LED, 12000);
+         gpio_put(GREEN_LED, false);
+ 
+         sleep_ms(LED_DELAY_MS);
+ 
+         pwm_set_gpio_level(BLUE_LED, 3000);
+         gpio_put(GREEN_LED, true);
+ 
+         sleep_ms(LED_DELAY_MS);
+     }
+ }
+ 
