22bdb8809cc0f140cce0a4fabe26f685810db7cd
notMarque <119892718+notMarque@users.noreply.github.com>
2025-05-05 19:01:32 -0400

    imports

--- blink.c
+++ blink.c
@@ -1,4 +1,8 @@
 #include "pico/stdlib.h"
+#include <stdio.h>
+#include <hardware/i2c.h>
+#include <pico/i2c_slave.h>
+#include "hardware/clocks.h"
 
 #define LED_DELAY_MS 500
 #define BLUE_LED 21
@@ -7,6 +11,8 @@
 
 
 int main() {
+    stdio_init_all();
+
     gpio_init(BLUE_LED);
     gpio_set_dir(BLUE_LED, GPIO_OUT);
 
@@ -23,5 +29,9 @@ int main() {
         gpio_put(GREEN_LED, true);
 
         sleep_ms(LED_DELAY_MS);
+        
+        uint32_t sys_clk_hz = clock_get_hz(clk_sys);
+
+        printf("RP2040 System Clock Frequency: %lu Hz\n", sys_clk_hz);
     }
 }
