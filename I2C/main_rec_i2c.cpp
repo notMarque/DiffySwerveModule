cac3be9e7a0a75cfdf449a20a3447d370f81a06b
notMarque <119892718+notMarque@users.noreply.github.com>
2025-05-07 09:43:16 -0400

    added esp connection stuff

---
+++ I2C/main_rec_i2c.cpp
@@ -0,0 +1,70 @@
+#include <main_rec_i2c.h>
+
+// The slave implements a 256 byte memory. To write a series of bytes, the master first
+// writes the memory address, followed by the data. The address is automatically incremented
+// for each byte transferred, looping back to 0 upon reaching the end. Reading is done
+// sequentially from the current memory address.
+static struct
+{
+    uint8_t mem[256];
+    uint8_t mem_address;
+    bool mem_address_written;
+} context;
+
+// Our handler is called from the I2C ISR, so it must complete quickly. Blocking calls /
+// printing to stdio may interfere with interrupt handling.
+void main_rec_i2c::i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
+    switch (event) {
+    case I2C_SLAVE_RECEIVE: // master has written some data
+        if (!context.mem_address_written) {
+            // writes always start with the memory address
+            context.mem_address = i2c_read_byte_raw(i2c);
+            context.mem_address_written = true;
+        } else {
+            // save into memory
+            context.mem[context.mem_address] = i2c_read_byte_raw(i2c);
+            context.mem_address++;
+        }
+        break;
+    case I2C_SLAVE_REQUEST: // master is requesting data
+        // load from memory
+        i2c_write_byte_raw(i2c, context.mem[context.mem_address]);
+        context.mem_address++;
+        break;
+    case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
+        context.mem_address_written = false;
+        break;
+    default:
+        break;
+    }
+}
+
+void main_rec_i2c::setup_slave(uint address) {
+    gpio_init(I2C_SLAVE_SDA_PIN);
+    gpio_set_function(I2C_SLAVE_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SLAVE_SDA_PIN);
+
+    gpio_init(I2C_SLAVE_SCL_PIN);
+    gpio_set_function(I2C_SLAVE_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SLAVE_SCL_PIN);
+
+    i2c_init(i2c0, I2C_BAUDRATE);
+    // configure I2C0 for slave mode
+    i2c_slave_init(i2c0, address, &i2c_slave_handler);
+}
+
+void main_rec_i2c::read_message(BotState& state) {
+    uint8_t msgLength = sizeof(BotState);
+    memcpy(&state, context.mem, msgLength);
+    printf("%d", state.u);
+    puts("\n");
+    printf("%d", state.v);
+    puts("\n");
+    printf("%d", state.w);
+    puts("\n");
+}
+
+void main_rec_i2c::save_to_mem(BotState& state) {
+    uint8_t msgLength = sizeof(state);
+    memcpy(&context.mem[14], &state, msgLength);
+}
--- blink.cpp
+++ blink.cpp
@@ -6,6 +6,7 @@
 #include <maini2c.h>
 #include <modulei2c.h>
 #include <structs.h>
+#include <main_rec_i2c.h>
 
 
 int main() {
@@ -13,25 +14,34 @@ int main() {
 
     puts("\nI2C slave example");
 
-    maini2c hub;
-    modulei2c module;
-    ModuleState state = ModuleState(1, 2, 3, 4, 5);
-    ModuleState returnState = ModuleState();
-
+    //maini2c hub;
+    //modulei2c module;
+    main_rec_i2c main_rec;
+    //ModuleState state = ModuleState(1, 2, 3, 4, 5);
+    //ModuleState returnState = ModuleState();
+    BotState botSt = BotState();
+    
 
     //hub.setup_slave();
-    module.setup_slave(0x17);
-    hub.init_master();
+    //module.setup_slave(0x17);
+    //hub.init_master();
+    main_rec.setup_slave(0x16);
     //thisi2c.run_master();
     for (int i = 0; i < 30; i++) {
         sleep_ms(100);
         puts("howdy");
     }
-    hub.send_message(state, 0x17);
-    module.save_to_mem(state);
+    //hub.send_message(state, 0x17);
+    //module.save_to_mem(state);
     sleep_ms(100);
-    module.read_message(state);
-    hub.read_message(returnState, 0x17);
+    //module.read_message(state);
+    //hub.read_message(returnState, 0x17);
+    //main_rec.read_message(botSt);
 
+    while (true) {
+        //module.read_message(state);
+        main_rec.read_message(botSt);
+        sleep_ms(100);
+    }
     
 }
